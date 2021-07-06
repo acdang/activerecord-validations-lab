018ae8b55e9e3c001f54600a3b715f5c2435c692
Anh Dang <anhcdang@gmail.com>
2021-07-05 20:44:20 -0700

    Done.

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,5 +1,6 @@
 class TitleValidator < ActiveModel::Validator
     def validate(record)
+        # need to make sure record.title itself is not nil
         if !record.title || !(["Won't Believe", "Secret", "Top \d", "Guess"].any? { |fragment| record.title.include? fragment })
             record.errors[:title] << "Title must be sufficiently clickbait-y."
         end
